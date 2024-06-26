a9fe9532e10cf75cafe38c6a0254d7a258bd907f
Seyma Bayezit <163021404+seymabyzt@users.noreply.github.com>
2024-06-26 17:56:52 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,110 @@
+import { useState, useEffect } from "react";
+import styles from "./App.module.css";
+import WeatherCard from "./components/organisms/WeatherInfo/WeatherCard";
+import MiniCard from "./components/organisms/WeatherInfo/MiniCard";
+import { getWeatherData } from "./services/api";
+import SearchBar from "./components/molecules/SearchBar/SearchBar";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [weather, setWeather] = useState({});
+  const [values, setValues] = useState([]);
+  const [place, setPlace] = useState("ANKARA");
+  const [thisLocation, setLocation] = useState("");
+
+  const parseDate = (dateString) => {
+    const [day, month, year] = dateString.split(".");
+    return new Date(year, month - 1, day);
+  };
+
+  const fetchWeather = async () => {
+    try {
+      const data = await getWeatherData(place);
+      const currentWeather = data.result[0];
+
+      const dailyForecast = data.result.map((entry) => ({
+        datetime: parseDate(entry.date),
+        temp: entry.degree,
+        description: entry.description,
+        icon: entry.icon,
+        min: entry.min,
+        max: entry.max,
+        status: entry.status
+      }));
+      const location = data.city;
+
+      setWeather(currentWeather);
+      setValues(dailyForecast);
+      setLocation(location);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+      alert("An error occurred while fetching the weather data.");
+    }
+  };
+
+  const handleInputChange = (e) => {
+    let sanitizedInput = e.target.value.toUpperCase().replace(/[^A-Z]/g, "");
+    if (sanitizedInput.length <= 30) {
+      setInput(sanitizedInput);
+    }
+  };
+
+  const handleKeyUp = (e) => {
+    if (e.key === "Enter") {
+      submitCity();
+    }
+  };
+
+  const submitCity = () => {
+    setPlace(input);
+    setInput("");
+  };
+
+  useEffect(() => {
+    fetchWeather();
+  }, [place]);
+
+  return (
+    <div className={styles.background}>
+      <div className={styles.overlay}>
+      <div className={styles.navContainer}>
+        <nav className={styles.navBar}>
+          <h1 className={styles.title}>Weather App</h1>
+          <SearchBar
+            value={input}
+            onChange={handleInputChange}
+            onKeyUp={handleKeyUp}
+            placeholder="Search city"
+          />
+        </nav>
+      </div>
+      <div className={styles.mainContent}>
+        <WeatherCard
+          place={thisLocation}
+          humidity={weather.humidity}
+          temperature={weather.degree}
+          icon={weather.icon}
+          description={weather.description}
+          night={weather.night}
+          status={weather.status}
+          min={weather.min}
+          max={weather.max}
+        />
+        <div className={styles.miniCards}>
+          {values.map((daily) => (
+            <MiniCard
+              key={daily.datetime}
+              time={daily.datetime}
+              temp={daily.temp}
+              icon={daily.icon}
+              description={daily.description}
+            />
+          ))}
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
